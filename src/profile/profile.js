e7603801f6323cc7d5b66627c0f112aa0065e3fb
Matthew Szeto <ms8288@gmail.com>
2020-02-23 21:55:53 -0800

    started profile page

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -1,7 +1,32 @@
-import React, { Component } from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Avatar from '@material-ui/core/Avatar';
 
-export default class ProfileComponent extends Component {
-	render() {
-		return <div>Profile</div>;
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(5, 15),
+      width: theme.spacing(100),
+      height: theme.spacing(100),
+	},
+	paper: {
+		display: 'flex',
+		margin: theme.spacing(5,5),
 	}
+  },
+}));
+
+export default function SimplePaper() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+    	<Paper elevation={5}>
+			<Avatar className={classes.paper}/>
+		</Paper>
+    </div>
+  );
 }

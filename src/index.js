37fb7f0f031b1ae5b71469620629d7eb98c5b18c
Steven <steven.chung96@yahoo.com>
2020-02-23 19:23:58 -0800

    redirected default route

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Redirect } from "react-router-dom";
 import LoginComponent from "./login/login";
 import SignupComponent from "./signup/signup";
 import DashboardComponent from "./dashboard/dashboard";
@@ -12,6 +12,7 @@ import AboutComponent from "./about/about";
 const routing = (
 	<Router>
 		<div id="routing-container">
+			<Redirect exact from="/" to="/login" />
 			<Route path="/login" component={LoginComponent}></Route>
 			<Route path="/signup" component={SignupComponent}></Route>
 			<Route path="/dashboard" component={DashboardComponent}></Route>

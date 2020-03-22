638eebf3f33efa10f3051068cd45ca9283a1f38a
Steven <steven.chung96@yahoo.com>
2020-03-22 11:13:51 -0700

    click and drag function working

--- src/whiteboard/canvas.js
+++ src/whiteboard/canvas.js
@@ -4,40 +4,72 @@ import io from 'socket.io-client';
 
 class Canvas extends Component {
 
-    // constructor(props) {
-    //     super(props);
+    constructor(props) {
+        super(props);
+        // this.state = {
+        //     isMouseDown: false
+        // }
 
-    //     this.state = { x: 0, y: 0 };
+        this._onMouseMove = this._onMouseMove.bind(this);
+    }
 
-    //     this.mouseDragged = this.mouseDragged.bind(this);
-    // }
+    isMouseDown = false;
 
     // // creating a variable named socket
-    // socket;
+    socket;
 
     // componentDidMount() {
     //     this.socket = io.connect('http://localhost:4000')
     // }
 
-    // // getting the X and Y positions when mouse is dragged, then send data to the socket
-    // mouseDragged(e) {
-    //     console.log('mouse dragging...');
-    //     this.setState({ x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY });
-    //     console.log('Sending: ' + this.state.x + ', ' + this.state.y);
+    // when mouse is clicked
+    _onMouseDown = (e) => {
+        // this.setState({isMouseDown: true});
+        this.isMouseDown = true;
+    }
 
-    //     this.socket.emit('mouse', data);
-    // }
-    
+    // getting the X and Y positions when mouse is dragged, then send data to the socket
+    _onMouseMove = (e) => {
+        if(this.isMouseDown === true) {
+            const width = this.refs.whiteboard.clientWidth;
+            const height = this.refs.whiteboard.clientHeight;
+            const oX = e.nativeEvent.offsetX;
+            const oY = e.nativeEvent.offsetY;
+
+            console.log(Math.floor(oX), Math.floor(oY));
+
+            var data = { 
+                x: oX,
+                y: oY
+            }
+
+            // this.socket.emit('mouse', data);
+        }
+    }
+
+    _onMouseUp = () => {
+        // this.setState({isMouseDown: false});
+        this.isMouseDown = false;
+    }
+
     render() {
+
         return (
-            <SketchField
-            width='1024px'
-            height='768px'
-            tool={Tools.Pencil}
-            lineColor='black'
-            lineWidth={3}
-            backgroundColor='#dcdde1'
-            />
+            <div className="whiteboard"
+            onMouseMove={ this._onMouseMove }
+            onMouseDown={ this._onMouseDown }
+            onMouseUp={ this._onMouseUp }
+            ref="whiteboard"
+            >
+                <SketchField
+                width='975px'
+                height='650px'
+                tool={Tools.Pencil}
+                lineColor='black'
+                lineWidth={3}
+                backgroundColor='#dcdde1'
+                />
+            </div>
         );
     }
 }

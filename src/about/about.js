3f2dd8814ba04170cc8212ec4b6dfc2d4bcba77c
Steven <steven.chung96@yahoo.com>
2020-05-14 08:39:13 -0700

    about page edit

--- src/about/about.js
+++ src/about/about.js
@@ -14,29 +14,33 @@ class AboutComponent extends Component {
 			<div className="">
 				<NavBarComponent />
 				<div className="our_story">
-					<h1>Our Story</h1>
+					<h2>Our Story</h2>
 					<p>
 						Here at Ghostcord we strive to create a community through a platform where people can connect on another level, from anywhere in the world. We enable user to technologically interact with each other’s worlds through modern technology while delivering a satisfying experience. Ghostcord will move technological communication to a whole other dimension. With just a few months since deployment, Ghostcord has become one of the industry leading companies placing us with Snapchat, Instagram, and Discord.
 					</p>
 				</div>
 				<div className="founders">
 					<h2>The Founders</h2>
-					<div className="col">
-						<h4>Steven Chung</h4>
-						<h4>Henry Hong</h4>
-					</div>
-					<div className="col">
-						<h4>Dung Nguyen</h4>
-						<h4>Mattew Szeto</h4>
+					<div className="names">
+						<div className="col">
+							<h4>Steven Chung</h4>
+							<h4>Henry Hong</h4>
+						</div>
+						<div className="col">
+							<h4>Dung Nguyen</h4>
+							<h4>Mattew Szeto</h4>
+						</div>
 					</div>
 				</div>
 				<div className="contact">
 					<h2>Contact Us</h2>
-					<div className="col">
-						<p><span>Email:</span>
-							<a href=""> ghostcord@gmail.com</a>
-						</p>
-						<p><span>Phone:</span> 453-528-2188</p>
+					<div className="info">
+						<div className="col">
+							<p><span>Email:</span>
+								<a href=""> ghostcord@gmail.com</a>
+							</p>
+							<p><span>Phone:</span> 453-528-2188</p>
+						</div>
 					</div>
 				</div>
 				<div>

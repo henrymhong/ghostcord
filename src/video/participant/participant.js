7d2a7e1c6341ebbf6cd5b54089affefd95d253d2
Matthew Szeto <ms8288@gmail.com>
2020-05-14 15:13:31 -0700

    added audio tracks

--- src/video/participant/participant.js
+++ src/video/participant/participant.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
 const Participant = ({ participant }) => {
   const [videoTracks, setVideoTracks] = useState([]);
@@ -7,33 +7,33 @@ const Participant = ({ participant }) => {
   const videoRef = useRef();
   const audioRef = useRef();
 
-  const trackpubsToTracks = trackMap => Array.from(trackMap.values())
-  .map(publication => publication.track)
-  .filter(track => track !== null);
-
+  const trackpubsToTracks = (trackMap) =>
+    Array.from(trackMap.values())
+      .map((publication) => publication.track)
+      .filter((track) => track !== null);
 
   useEffect(() => {
-    const trackSubscribed = track => {
-      if (track.kind === 'video') {
-        setVideoTracks(videoTracks => [...videoTracks, track]);
-      } else {
-        setAudioTracks(audioTracks => [...audioTracks, track]);
-      }
-    };
-
-    const trackUnsubscribed = track => {
-      if (track.kind === 'video') {
-        setVideoTracks(videoTracks => videoTracks.filter(v => v !== track));
-      } else {
-        setAudioTracks(audioTracks => audioTracks.filter(a => a !== track));
-      }
-    };
-
     setVideoTracks(trackpubsToTracks(participant.videoTracks));
     setAudioTracks(trackpubsToTracks(participant.audioTracks));
 
-    participant.on('trackSubscribed', trackSubscribed);
-    participant.on('trackUnsubscribed', trackUnsubscribed);
+    const trackSubscribed = (track) => {
+      if (track.kind === "video") {
+        setVideoTracks((videoTracks) => [...videoTracks, track]);
+      } else if (track.kind === "audio") {
+        setAudioTracks((audioTracks) => [...audioTracks, track]);
+      }
+    };
+
+    const trackUnsubscribed = (track) => {
+      if (track.kind === "video") {
+        setVideoTracks((videoTracks) => videoTracks.filter((v) => v !== track));
+      } else if (track.kind === "audio") {
+        setAudioTracks((audioTracks) => audioTracks.filter((a) => a !== track));
+      }
+    };
+
+    participant.on("trackSubscribed", trackSubscribed);
+    participant.on("trackUnsubscribed", trackUnsubscribed);
 
     return () => {
       setVideoTracks([]);
@@ -52,15 +52,21 @@ const Participant = ({ participant }) => {
     }
   }, [videoTracks]);
 
-  const isMute = false;
-
-
+  useEffect(() => {
+    const audioTrack = audioTracks[0];
+    if (audioTrack) {
+      audioTrack.attach(audioRef.current);
+      return () => {
+        audioTrack.detach();
+      };
+    }
+  }, [audioTracks]);
 
   return (
     <div className="participant">
       <h3>{participant.identity}</h3>
       <video ref={videoRef} autoPlay={true} />
-      <audio ref={audioRef} autoPlay={true}/>
+      <audio ref={audioRef} autoPlay={true} />
     </div>
   );
 };
